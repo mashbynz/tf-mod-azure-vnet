31f785bd7fd2bf9c65a360c1bd6198b082aae98b
matt ashby <matt@ashbys.nz>
2019-07-31 16:12:38 +1200

    update label

--- label.tf
+++ label.tf
@@ -105,11 +105,3 @@ module "vgw_label" {
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
-
-module "ipconfig_label" {
-  source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
-  context            = var.context
-  attributes         = ["gw"]
-  delimiter          = "-"
-  additional_tag_map = {} /* Additional attributes (e.g. 1) */
-}
