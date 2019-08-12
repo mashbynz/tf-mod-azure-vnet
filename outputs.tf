e4beabd980d83d653e13332e7b40c9a160de93d1
Matt Ashby <matt.ashby@theinstillery.com>
2019-08-13 10:21:54 +1200

    gw update

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "rg_name" {
   value = azurerm_resource_group.default.name
 }
 
+output "rg_id" {
+  value = azurerm_resource_group.default.id
+}
+
+output "rg_location" {
+  value = azurerm_resource_group.default.location
+}
+
 output "vnet_id" {
   value = azurerm_virtual_network.default.id
 }
