9fdda4b88ddfadce70ed68c528e0eb7e6aa91017
callumccr1 <callumccr@gmail.com>
2019-08-27 10:47:33 +1200

    test

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "rg_name" {
-  value = list(azurerm_resource_group.default.*.name)
+  value = azurerm_resource_group.default.*.name
 }
 
 output "rg_id" {
-  value = list(azurerm_resource_group.default.*.id)
+  value = azurerm_resource_group.default.*.id
 }
 
 output "rg_location" {
-  value = list(azurerm_resource_group.default.*.location)
+  value = azurerm_resource_group.default.*.location
 }
 
 output "vnet_id" {
-  value = list(azurerm_virtual_network.default.*.id)
+  value = azurerm_virtual_network.default.*.id
 }
 
 output "gateway_subnet_id" {
-  value = list(azurerm_subnet.gateway.*.id)
+  value = sazurerm_subnet.gateway.*.id
 }
